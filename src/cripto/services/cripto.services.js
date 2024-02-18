6f96b072000746dba6336262e29b1b0b90c9c777
Matias Ramirez <ramirezmatias946@gmail.com>
2024-02-18 18:47:55 -0300

    fix: agregamos await para obtener criptos externos

--- src/cripto/services/cripto.services.js
+++ src/cripto/services/cripto.services.js
@@ -2,7 +2,7 @@ const { default: axios } = require("axios");
 const getCriptosExterno = require("../data/cripto.data");
 
 async function getAllCriptos() {
-  const criptos = getCriptosExterno();
+  const criptos = await getCriptosExterno();
 
   const mappedCriptos = criptos.data.map((cripto) => {
     const { id, symbol, name, volumeUsd24Hr, priceUsd, changePercent24Hr } =
