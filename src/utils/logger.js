2f4e829d50dbaa82f331b1adff47aaaead7c8beb
Matias Ramirez <ramirezmatias946@gmail.com>
2024-02-18 17:44:40 -0300

    refactor: agregamos un logger

---
+++ src/utils/logger.js
@@ -0,0 +1,9 @@
+function logger(custom,err=""){
+    const NODE_ENV = process.env.NODE_ENV || 'dev';
+    if(NODE_ENV.toLowerCase() === 'dev'){
+        console.log(custom);
+        console.log(err);
+    }
+}
+
+module.exports = logger;
