e421a8c8140854a09b20fa814a0cb697aa2935fe
Matias Ramirez <ramirezmatias946@gmail.com>
2024-02-18 17:45:10 -0300

    refactor: aseguramos de manejar cualquier error en caso que no atrapemos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-const app = require('./server.js')
+const app = require("./server.js");
 
-const PORT = process.env.PORT  || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT,(req,res)=>{
-    console.log(`Servidor corriendo en http://127.0.0.1:${PORT}`)
+app.listen(PORT, (req, res) => {
+  console.log(`Servidor corriendo en http://127.0.0.1:${PORT}`);
+});
+
+// Manejo de errores no capturados
+
+process.on("uncaughtException", (error) => {
+  console.error("uncaughtException", error);
+  process.exit(1);
+});
+
+// Manejo de promesas no capturadas
+process.on("unhandledRejection", (error) => {
+  console.error("unhandledRejection", error);
+  process.exit(1);
 });
